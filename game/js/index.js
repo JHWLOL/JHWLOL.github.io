37f818d563ca3e1d577eff4939745254d5ec5ee6
jack woolterton <jack.woolterton@yahoo.co.uk>
2016-06-07 16:10:51 +0800

    added game

---
+++ game/js/index.js
@@ -0,0 +1,31 @@
+function playGame() {
+
+alert("You're a old lord of the cinammon hills. ")
+
+var question1=prompt('You see a portal!Do you go or stay');
+if (question1==="go"){
+  alert('you get transported to the world of cinamon');
+} 
+else {
+  alert('you stay at home and live untill you die. you eat cinnammon from your hill. you eat it everyday til there is no hill left then it crushes you taking you to a unkown land');
+} 
+
+var question2=prompt('You see a enemy !Do you fight or run');
+
+if (question2==="fight"){
+  alert('you kill the weakling');
+} 
+else {
+  alert('you run away you chicken. you  get cinamon pocks for bieing a chicken');
+}
+  
+  var question3=prompt('You see a Boss !Do you fight or run');
+  
+if (question3==="fight"){
+  alert('The boss kills you with his cinamon club');
+}
+ else {
+  alert('You beat the game');
+ }
+  alert("thanks for playing!")
+}
